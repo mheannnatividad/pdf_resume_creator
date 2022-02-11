e37d856fd271eb62010f8213bc8e06af9ba77fec
Mary Ann Natividad <maryannbernardo212@gmail.com>
2022-02-12 00:33:29 +0800

    done

---
+++ pdfResumeCreator/pdfResumeCreator/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace pdfResumeCreator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Createbtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Createbtn
+            // 
+            this.Createbtn.Location = new System.Drawing.Point(47, 163);
+            this.Createbtn.Name = "Createbtn";
+            this.Createbtn.Size = new System.Drawing.Size(331, 51);
+            this.Createbtn.TabIndex = 0;
+            this.Createbtn.Text = "Convert to PDF";
+            this.Createbtn.UseVisualStyleBackColor = true;
+            this.Createbtn.Click += new System.EventHandler(this.Createbtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Thistle;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(52, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(355, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "PDF RESUME CREATOR";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(111, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(223, 28);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Upload Resume here";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 253);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Createbtn);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Createbtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ pdfResumeCreator/pdfResumeCreator/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.draw;
+using System.IO;
+
+namespace pdfResumeCreator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Createbtn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Please Wait");
+            string jsonpathfile = @"C:\Users\Computer\source\repos\pdfResumeCreator\pdfResumeCreator\pdfresumecreator.json";
+            string openfile = File.ReadAllText(jsonpathfile);
+            jsonreader jsonoutput = JsonConvert.DeserializeObject<jsonreader>(openfile);
+            Document resume = new Document();
+            PdfWriter.GetInstance(resume, new FileStream(@"C:\Users\Computer\source\repos\pdfResumeCreator\pdfResumeCreator\NATIVIDAD_MARYANN.pdf",FileMode.Create));
+            LineSeparator separate = new LineSeparator(3f, 100f, BaseColor.BLACK, Element.ALIGN_CENTER, 1);
+            iTextSharp.text.Image image = iTextSharp.text.Image.GetInstance(jsonoutput.image);
+            image.ScalePercent(14f);
+            image.Alignment = 6;
+        
+
+
+            Paragraph myname = new Paragraph(jsonoutput.Full_name+"\n");
+            Paragraph add = new Paragraph(jsonoutput.Address);
+            Paragraph Phnum = new Paragraph(jsonoutput.Phone_number);
+            Paragraph telnum = new Paragraph(jsonoutput.Tel_number);
+            Paragraph emailadd = new Paragraph(jsonoutput.email_address+"\n\n");
+            Paragraph educattain = new Paragraph("Educational Attainment:\n\n"+jsonoutput.Tertiary+"\n\n"+jsonoutput.Secondary+"\n\n"+jsonoutput.Elementary+"\n\n");
+            Paragraph workexp = new Paragraph("Work Experience:\n\n" + jsonoutput.Position +jsonoutput.Company_Name + "\n\n"+jsonoutput.Position1 + jsonoutput.Company_Name1+"\n\n");
+            Paragraph prsnldata = new Paragraph("Personal Data:\n\n" + jsonoutput.nick_name + "\n\n" + jsonoutput.height + "\n\n" + jsonoutput.citizenship + "\n\n" + jsonoutput.date_of_birth + "\n\n" +
+                jsonoutput.religion + "\n\n" + jsonoutput.Language);
+
+            resume.Open();
+            resume.Add(image);
+            resume.Add(myname);
+            resume.Add(add);
+            resume.Add(Phnum);
+            resume.Add(telnum);
+            resume.Add(emailadd);
+            resume.Add(separate);
+            resume.Add(educattain);
+            resume.Add(separate);
+            resume.Add(workexp);
+            resume.Add(separate);
+            resume.Add(prsnldata);
+            resume.Close();
+
+        }
+        public class jsonreader
+        {
+            public string Full_name
+            {
+                get; set;
+            }
+           public string image
+            {
+                get; set;
+            }
+            public string Phone_number
+            {
+                get; set;
+            }
+            public string Address
+            {
+                get; set;
+            }
+            public string Tel_number
+            {
+                get; set;
+            }
+            public string email_address
+            {
+                get; set;
+            }
+            public string Secondary
+            {
+                get; set; 
+            }
+            public string Tertiary
+            {
+                get; set;
+            }
+            public string Elementary
+            {
+                get; set;
+            }
+            public string Position
+            {
+                get; set;
+            }
+            public string Company_Name
+            {
+                get; set;
+            }
+            public string Position1
+            {
+                get; set;
+            }
+            public string Company_Name1
+            {
+                get; set;
+            }
+            public string nick_name
+            {
+                get; set;
+            }
+            public string height
+            {
+                get; set;
+            }
+            public string citizenship
+            {
+                get; set;
+            }
+            public string date_of_birth
+            {
+                get; set;
+            }
+            public string religion
+            {
+                get; set;
+            }
+            public string Language
+            {
+                get; set;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
